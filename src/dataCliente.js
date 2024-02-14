ed7220406937e1f0087f67357ae95aa5f1a8c2db
Marcelo Medel <marcelo.medel.v@gmail.com>
2024-02-14 17:49:13 -0300

    update index.html, data, isologo @

--- src/dataCliente.js
+++ src/dataCliente.js
@@ -3,13 +3,13 @@ export const dataCliente = [
         title: 'Toyota RAV4 Híbrida',
         description: 'El car detailing es un servicio profesional que deja tu automóvil como nuevo',
         before: "https://images.unsplash.com/photo-1622210642960-0f6a2cdbdc9f?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHNlYXJjaHwxNHx8cmF2NHxlbnwwfHx8fDE3MDc4Mjk1MzN8MA&ixlib=rb-4.0.3&q=85&w=1920",
-        after: "https://images.unsplash.com/photo-1622210642960-0f6a2cdbdc9f?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHNlYXJjaHwxNHx8cmF2NHxlbnwwfHx8fDE3MDc4Mjk1MzN8MA&ixlib=rb-4.0.3&q=85&w=1920"
+        after: "https://scontent.fscl9-2.fna.fbcdn.net/v/t1.6435-9/43739982_1947137572047738_7152721179072004096_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=c2f564&_nc_ohc=I_E4J4Dw2JsAX8Z7-FY&_nc_ht=scontent.fscl9-2.fna&oh=00_AfDg6unBgPzVRxadzFqGd3TDHyJ2L6iU7vH0yYLN6HUweg&oe=65F464B1"
     },
     {
     
         title: 'Ford F150',
         description: 'El car detailing es un servicio profesional que deja tu automóvil como nuevo',
         before: "https://scontent.fscl9-2.fna.fbcdn.net/v/t1.6435-9/43739982_1947137572047738_7152721179072004096_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=c2f564&_nc_ohc=I_E4J4Dw2JsAX8Z7-FY&_nc_ht=scontent.fscl9-2.fna&oh=00_AfDg6unBgPzVRxadzFqGd3TDHyJ2L6iU7vH0yYLN6HUweg&oe=65F464B1",
-        after: "https://scontent.fscl9-2.fna.fbcdn.net/v/t1.6435-9/43739982_1947137572047738_7152721179072004096_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=c2f564&_nc_ohc=I_E4J4Dw2JsAX8Z7-FY&_nc_ht=scontent.fscl9-2.fna&oh=00_AfDg6unBgPzVRxadzFqGd3TDHyJ2L6iU7vH0yYLN6HUweg&oe=65F464B1"
+        after: "https://images.unsplash.com/photo-1622210642960-0f6a2cdbdc9f?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHNlYXJjaHwxNHx8cmF2NHxlbnwwfHx8fDE3MDc4Mjk1MzN8MA&ixlib=rb-4.0.3&q=85&w=1920"
     }
 ]
