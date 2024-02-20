fd1325fd83f4837b2927c98d7fec140698c675c1
Marcelo Medel <marcelo.medel.v@gmail.com>
2024-02-20 18:25:06 -0300

    se incorpora slider de imagenes en clientes

--- src/assets/components/ClienteBeforeAfter.jsx
+++ src/assets/components/ClienteBeforeAfter.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { ImgComparisonSlider } from '@img-comparison-slider/react';
 //import '@img-comparison-slider/react/dist/styles.css'; // Ensure you import the CSS
@@ -15,9 +17,9 @@ export default function ClienteBeforeAfter({ title, description, before, after }
   return (
 
 
-    <div class="w-full lg:w-1/2 px-4 mb-20">
-      <a class="group block" href="#">
-        <div class="relative mb-6 rounded-4xl overflow-hidden">
+    <div className="w-full lg:w-1/2 px-4 mb-20" >
+      <a className="group block" href="#">
+        <div className="relative mb-2 react-img-comparison object-cover">
           {/* <div class="absolute left-0 top-0 h-full w-full group-hover:bg-opacity-10 group-hover:bg-black transition duration-200"></div> */}
           <ImgComparisonSlider>
             <img slot="first" src={before} style={imageStyles} />
@@ -26,30 +28,11 @@ export default function ClienteBeforeAfter({ title, description, before, after }
         </div>
         <div>
           
-          <h3 class="text-4xl tracking-tight mb-6">{title}</h3>
-          <p class="max-w-xl text-coolGray-600">{description}</p>
+          <h3 className="text-4xl tracking-tight mb-6">{title}</h3>
+          <p className="max-w-xl text-coolGray-600">{description}</p>
         </div>
       </a>
     </div>
 
-
-    // <div className='flex-row flex-wrap w-full lg:w-1/2 px-4 mb-20 p-2'>
-    //   <ImgComparisonSlider>
-    //     <img slot="first" src={before} style={imageStyles} />
-    //     <img slot="second" src={after} style={imageStyles} />
-    //   </ImgComparisonSlider>
-    //   <div>
-    //     <h3 className='text-4xl tracking-tight mb-6'>
-    //       {title}
-    //     </h3>
-    //     <p
-    //       className='max-w-xl text-coolGray-600'
-    //       contentEditable='false'
-    //     >
-    //       {description}
-    //     </p>
-    //   </div>
-
-    // </div>
   );
 }
--- src/pages/clientes.jsx
+++ src/pages/clientes.jsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
 import Header from '../assets/components/Header'
 import ClientesGallery from '../assets/components/ClientesGallery'
 import Contacto from '../assets/components/Contacto'
