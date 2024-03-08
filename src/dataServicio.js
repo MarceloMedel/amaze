8f01398865de35a2f6740257fc9bbf7c094c491a
Marcelo Medel <marcelo.medel.v@gmail.com>
2024-03-08 07:33:05 -0300

    launch v2

--- src/dataServicio.js
+++ src/dataServicio.js
@@ -3,28 +3,28 @@ export const dataServicio = [
         title: "Car Detailing",
         description: 'Eleva tu experiencia al volante con nuestro servicio de car detailing, una transformación integral que va más allá de la limpieza para restaurar y proteger tu vehículo por dentro y por fuera. Nuestro meticuloso proceso elimina contaminantes y rayas, aplica sellado cerámico para enfrentar elementos adversos, y detalla interiores para una belleza duradera.',
         icon: 'images/detallado-icono-svg-1707782837412.webp',
-        image: 'https://images.unsplash.com/photo-1632823469901-5d2cfff5ba50?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHNlYXJjaHwxNHx8Y2FyJTIwZGV0YWlsaW5nfGVufDB8fHx8MTcwNzc4MzAwNnww&ixlib=rb-4.0.3&q=85&w=1920',
+        image: 'images/pulido2.jpg',
         alt: ''
     },
     {   
         title: "PPF y Wrapping",
         description: 'Transforma y protege tu auto con nuestros servicios avanzados de wrapping y PPF, diseñados para realzar tu estilo personal y ofrecer una protección superior contra daños. Utilizando materiales de primera calidad, nuestros servicios no solo mejoran la estética de tu vehículo, sino que también aseguran su preservación a largo plazo contra rasguños y elementos externos. Perfecto para mantener tu auto en su mejor versión, visual y funcionalmente.',
         icon: '',
-        image: '../public/images/wrapping.jpg',
+        image: 'images/wrapping.jpg',
         alt: ''
     },
     {   
         title: "Premium Car Wash",
         description: 'Experimenta el máximo nivel de cuidado automotriz con nuestro servicio premium de car wash, que va más allá de un lavado convencional para ofrecerte una limpieza profunda y un realce de brillo excepcional. Perfecto para el mantenimiento después del sellado cerámico, este servicio garantiza la durabilidad de tu inversión, preservando la protección y estética de tu vehículo. ',
         icon: '',
-        image: 'https://images.unsplash.com/photo-1641323990095-2110680fe979?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMzIzMzB8MHwxfHNlYXJjaHwyMHx8Y2FyJTIwd2FzaHxlbnwwfHx8fDE3MDc3ODMwNzB8MA&ixlib=rb-4.0.3&q=85&w=1920',
+        image: 'images/carwash.jpg',
         alt: ''
     },
     {   
         title: "Pintado de Calipers y Llantas",
         description: 'Dale un toque personal y distintivo a tu auto con nuestro servicio de pintado de calipers y llantas, diseñado para reflejar tu estilo único. Con técnicas precisas y pinturas de alta calidad, aseguramos un acabado duradero y llamativo, transformando tu vehículo en una expresión de tu individualidad y una verdadera obra de arte en movimiento.',
         icon: '',
-        image: '../public/images/lamina-seguridad.jpg',
+        image: 'images/pintado-caliper.jpg',
         alt: ''
     }
 ]
